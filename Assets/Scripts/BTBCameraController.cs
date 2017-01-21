2ea3621fcc4e844fbc6dfee6caf386fd4eb45ccf
Javier Meza <javenmeza@gmail.com>
2017-01-21 09:48:27 -0600

    Switch to unity

---
+++ Assets/Scripts/BTBCameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BTBCameraController : MonoBehaviour {
+  private float initialY;
+
+	// Use this for initialization
+	void Start () {
+	  initialY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    transform.position = new Vector2(transform.position.x, initialY);
+	}
+}
---
+++ Assets/Scripts/BTBPlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody2D))]
+public class BTBPlayerController : MonoBehaviour {
+
+  public float MoveForce;
+
+  private Rigidbody2D rigidbody;
+
+	void Start () {
+	  rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+	  if(Input.GetKey(KeyCode.RightArrow)) {
+      rigidbody.AddForce(new Vector2(20, 0));
+    }
+
+    if(Input.GetKey(KeyCode.LeftArrow)) {
+      rigidbody.AddForce(new Vector2(-20, 0));
+    }
+	}
+}
