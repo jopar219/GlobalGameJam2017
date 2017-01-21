9155bf93e55d9a2bcb537f5321dd72a2e87f74de
julio177 <julioc.aguilarv@gmail.com>
2017-01-21 12:24:35 -0600

    Cambios para cambiar programaticamente el bounciness

--- Assets/Scripts/BTBCameraController.cs
+++ Assets/Scripts/BTBCameraController.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class BTBCameraController : MonoBehaviour {
   private float initialY;
 
-	// Use this for initialization
-	void Start () {
-	  initialY = transform.position.y;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-    transform.position = new Vector2(transform.position.x, initialY);
-	}
+  // Use this for initialization
+  void Start () {
+    initialY = transform.position.y;
+  }
+  
+  // Update is called once per frame
+  void Update () {
+      transform.position = new Vector2(transform.position.x, initialY);
+  }
 }
--- Assets/Scripts/BTBPlayerController.cs
+++ Assets/Scripts/BTBPlayerController.cs
@@ -8,17 +8,25 @@ public class BTBPlayerController : MonoBehaviour {
 
   private Rigidbody2D rigidbody;
 
-	void Start () {
-	  rigidbody = GetComponent<Rigidbody2D>();
-	}
-	
-	void Update () {
-	  if(Input.GetKey(KeyCode.RightArrow)) {
+  private float velY;
+  public float TimeTilNextBounce;
+
+  void Start () {
+    rigidbody = GetComponent<Rigidbody2D>();
+  }
+  
+  void Update () {
+    if(Input.GetKey(KeyCode.RightArrow)) {
       rigidbody.AddForce(new Vector2(20, 0));
     }
 
     if(Input.GetKey(KeyCode.LeftArrow)) {
       rigidbody.AddForce(new Vector2(-20, 0));
     }
-	}
+  }
+
+  void OnCollisionEnter2D(Collision2D other) {
+    velY = -0.5f * Physics2D.gravity.y * TimeTilNextBounce;
+    rigidbody.velocity = new Vector2(rigidbody.velocity.x, velY);
+  }
 }
