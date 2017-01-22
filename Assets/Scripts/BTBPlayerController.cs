f53ea0b15ece93d46afa13f6ce01923b1b3e6426
Jorge Paredes <jopar219@gmail.com>
2017-01-22 14:33:37 -0600

    Adds min speed

--- Assets/Scripts/BTBPlayerController.cs
+++ Assets/Scripts/BTBPlayerController.cs
@@ -6,6 +6,7 @@ public class BTBPlayerController : MonoBehaviour {
 
   public float MoveForce;
   public float MaxSpeed;
+  public float MinSpeed;
   public float XVelocityScaling;
 
   private Rigidbody2D playerRigidbody;
@@ -15,23 +16,27 @@ public class BTBPlayerController : MonoBehaviour {
   }
   
 	void Update () {
-	  if(Input.GetKey(KeyCode.Space)) {
-      if(playerRigidbody.velocity.x < MaxSpeed) {
-        playerRigidbody.AddForce(new Vector2(MoveForce, 0));
-      }
-    } else if(playerRigidbody.velocity.x > 0){
-      if(Mathf.Abs(playerRigidbody.velocity.x) > 0.01) {
-        playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x * XVelocityScaling , playerRigidbody.velocity.y);
-      } else {
-        playerRigidbody.velocity = new Vector2(0, playerRigidbody.velocity.y);
-      }
-    }
+		if (Input.GetKey (KeyCode.Space)) {
+			if (playerRigidbody.velocity.x < MaxSpeed) {
+				playerRigidbody.AddForce (new Vector2 (MoveForce, 0));
+			} else {
+				playerRigidbody.velocity = new Vector2(MaxSpeed, playerRigidbody.velocity.y);
+			}
+		} else if (playerRigidbody.velocity.x > MinSpeed) {
+			if (Mathf.Abs (playerRigidbody.velocity.x) > MinSpeed + 0.01) {
+				playerRigidbody.velocity = new Vector2 (MinSpeed+(playerRigidbody.velocity.x-MinSpeed) * XVelocityScaling, playerRigidbody.velocity.y);
+			} else {
+				playerRigidbody.velocity = new Vector2 (MinSpeed, playerRigidbody.velocity.y);
+			}
+		} else if(playerRigidbody.velocity.x < MinSpeed){
+			playerRigidbody.velocity = new Vector2(MinSpeed, playerRigidbody.velocity.y);
+		}
 
-    //Movement to the left
-    if(Input.GetKey(KeyCode.LeftArrow)) {
-      if(playerRigidbody.velocity.x > -MaxSpeed) {
-        playerRigidbody.AddForce(new Vector2(-MoveForce, 0));
+      //Movement to the left
+      if(Input.GetKey(KeyCode.LeftArrow)) {
+        if(playerRigidbody.velocity.x > -MaxSpeed) {
+          playerRigidbody.AddForce(new Vector2(-MoveForce, 0));
+        }
       }
-    }
   }
 }
