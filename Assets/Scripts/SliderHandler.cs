8d4ef88a57f70455c5f9fbfe9764e6f7584a3c22
Jorge Paredes <jopar219@gmail.com>
2017-01-22 00:15:47 -0600

    Adds track editor

---
+++ Assets/Scripts/SliderHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SliderHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public MusicRecorder recorder;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnBeginDrag(PointerEventData eventData){
+		recorder.SeekStart();
+	}
+	public void OnDrag(PointerEventData eventData){
+		recorder.Seek(GetComponent<Slider>().value);
+	}
+	public void OnEndDrag(PointerEventData eventData){
+		recorder.SeekEnd ();
+	}
+}
